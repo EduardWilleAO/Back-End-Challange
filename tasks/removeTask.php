6030bf1a6ce6667569301ca3f9428d36997d8074
Unknown <99051422@mydavinci.nl>
2020-04-24 12:45:34 +0200

    Remove task function
    function to remove task,
    this checks for both a private id from the task itself and for an id which is linked to the list,
    
    so a list can have for instance 2 tasks:
    Stand up,
    Walk,
    
    Stand up has:
    Binding id 33 (from list id) and private id of 1
    
    Walk has:
    Binding id 33 (from list id) and private id of 2

---
+++ tasks/removeTask.php
@@ -0,0 +1,40 @@
+<?php
+
+require "../connect.php";
+
+$sql = 'SELECT * FROM tasks WHERE task_id=:task_id AND binding_id=:binding_id';
+$stmt = $conn->prepare($sql);
+$stmt->BindParam(":task_id", $_GET["task_id"]);
+$stmt->BindParam(":binding_id", $_GET["binding_id"]);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $sql = "DELETE FROM tasks WHERE task_id=:task_id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":task_id", $_GET["task_id"]);
+    $stmt->execute();
+
+    $conn = null;
+
+    header("Location: ../index.php");
+}
+
+$conn = null;
+
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <link href="../css/stylesheet.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form method="post">
+        <?php foreach($result as $value){ ?>
+            <h2>Are you sure you want to delete "<?php print $value["name"]; ?>"?</h2>
+            <button type="submit" name="button" class="btn-danger">Delete "<?php print $value["name"]; ?>"</button>
+        <?php } ?>
+    </form>
+</body>
+</html>
