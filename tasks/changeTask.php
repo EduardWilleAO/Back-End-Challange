0bd7f48fffc5437a10555b3bc217317257e4283d
Unknown <99051422@mydavinci.nl>
2020-05-15 10:45:05 +0200

    Update Tasks + Sorting
    Last function of the project is to be able to edit the tasks which is now functional. Also added sorting to the tasks.

--- tasks/changeTask.php
+++ tasks/changeTask.php
@@ -1,8 +1,56 @@
+    <?php 
+
+require "../connect.php";
+
+$sql = 'SELECT * FROM tasks WHERE task_id=:id';
+$stmt = $conn->prepare($sql);
+$stmt->BindParam(":id", $_GET["id"]);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $sql = "UPDATE tasks SET name=:name, description=:description, status=:status, duration=:duration WHERE task_id=:id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(":id", $_GET["id"]);
+    $stmt->bindParam(":name", $_POST["name"]);
+    $stmt->bindParam(":description", $_POST["description"]);
+    $stmt->bindParam(":status", $_POST["status"]);
+    $stmt->bindParam(":duration", $_POST["duration"]);
+    $stmt->execute();
+
+    $conn = null;
+
+    header('Location: tasks.php?id='.$_GET["binding_id"]);
+}
+
+$conn = null;
+?>
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <link href="../css/stylesheet.css" rel="stylesheet" />
     <title></title>
 </head>
 <body>
+    <?php foreach($result as $row){ ?>
+        <label>Changing Task: <b><?php print $row["name"]; ?></b></label>
+        <form method="post">
+            <label>Task Name: </label><input name="name" value="<?php print $row['name']; ?>"><br>
+        
+            <label>Task Description: </label><input name="description" value="<?php print $row['description']; ?>"><br>
+        
+            <label>Task Status: 
+            <select name="status">
+                <option selected="selected">Not Started</option>
+                <option>In Progress</option>
+                <option>Finished</option>
+            </select><br>
+        
+            <label>Task Duration: </label><input name="duration" value="<?php print $row['duration']; ?>"><br>
+
+            <input type="submit">
+        </form>
+    <?php } ?>
+    <a href="tasks.php?id=<?php print $_GET['binding_id']; ?>" type="button" class="btn-danger">Go Back</a>
 </body>
 </html>
--- tasks/tasks.php
+++ tasks/tasks.php
@@ -1,6 +1,7 @@
 <?php
 
 require "../connect.php"; 
+require "../modal/modal.php";
 
 // Get list info
 $sql = "SELECT * FROM list WHERE id=:id";
@@ -9,8 +10,14 @@ $stmt->bindParam(":id", $_GET["id"]);
 $stmt->execute();
 $result = $stmt->fetchAll();
 
-// Get task info
-$sql = "SELECT * FROM tasks WHERE binding_id=:binding_id";
+if ($_GET["FilterOrStatus"] == "status"){
+    $sql = "SELECT * FROM tasks WHERE binding_id=:binding_id ORDER BY status";
+} else if ($_GET["FilterOrStatus"] == "filter"){
+    $sql = "SELECT * FROM tasks WHERE binding_id=:binding_id ORDER BY duration";
+} else {
+    $sql = "SELECT * FROM tasks WHERE binding_id=:binding_id";
+}
+
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(":binding_id", $_GET["id"]);
 $stmt->execute();
@@ -27,17 +34,18 @@ $conn = null;
 </head>
 <body>
     <?php foreach($result as $row){ ?><label>Tasks for: <b><?php print $row["name"]; ?></b></label><br><?php } ?>
-
     <?php foreach($result2 as $row){ ?>
         <?php print $row["name"]; ?> |
         <?php print $row["description"]; ?> |
         <?php print $row["status"]; ?> |
         <?php print $row["duration"]; ?>
         <a href="removeTask.php?task_id=<?php print $row['task_id']; ?>&binding_id=<?php print $row['binding_id']; ?>" type="button" class="btn-danger">Remove Task</a>
-        <a href="changeTask.php?id=<?php print $row['id']; ?>" type="button" class="btn-success">Change Task</a><br>
+        <a href="changeTask.php?id=<?php print $row['task_id']; ?>&binding_id=<?php print $row['binding_id']; ?>" type="button" class="btn-success">Change Task</a><br>
     <?php } ?>
 
     <?php foreach($result as $row){ ?>
+        <a href="tasks.php?id=<?php print $row["id"]; ?>&FilterOrStatus=status" class="" type="button">Sort by status</a>
+        <a href="tasks.php?id=<?php print $row["id"]; ?>&FilterOrStatus=filter" class="" type="button">Filter by</a>
         <a href="../index.php" type="button" class="btn-danger">Go Back</a>
         <a href="newTask.php?id=<?php print $row["id"]; ?>" type="button" class="btn-primary">Add Task</a>
     <?php } ?>
